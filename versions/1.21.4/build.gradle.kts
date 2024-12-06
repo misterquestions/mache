5e82d492f1f2485d1e3157bf0c7c0b4d56444e7f
Nassim Jahnke <nassim@njahnke.dev>
2024-12-06 20:33:19 +0100

    Update sculptor

--- versions/1.21.4/build.gradle.kts
+++ versions/1.21.4/build.gradle.kts
@@ -1,7 +1,7 @@
 import io.papermc.sculptor.shared.util.MinecraftJarType
 
 plugins {
-    id("io.papermc.sculptor.version") version "1.0.8"
+    id("io.papermc.sculptor.version") version "1.0.9"
 }
 
 val generateReportsProperty = providers.gradleProperty("generateReports")
